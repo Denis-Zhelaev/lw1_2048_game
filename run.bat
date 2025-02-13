604a09e351adfaba2924cb55b01881bb5f4a737e
Denis-Zhelaev <117642209+Denis-Zhelaev@users.noreply.github.com>
2025-02-14 00:42:55 +0300

    bat
    добавил бат файл

---
+++ run.bat
@@ -0,0 +1,8 @@
+docker build -t lw1-game .
+docker run -d -p 8081:8080 --name lw1-game-cont1 lw1-game
+docker run -d -p 8082:8080 --name lw1-game-cont2 lw1-game
+
+echo "check: "
+echo "1 container: http://localhost:8081"
+echo "2 container: http://localhost:8082"
+pause
